9842163d0b7328541b62af79061ba965e6ce7a75
CaptainOfHacks <stefan.stratulat1997@gmail.com>
2023-02-14 13:14:50 +0200

    Update __init__.py

--- mapping_workbench/__init__.py
+++ mapping_workbench/__init__.py
@@ -2,7 +2,7 @@ __version__ = "0.0.1"
 
 import os
 
-RUN_ENV_NAME = "__TED_SWS_RUN_ENV__"
-RUN_ENV_VAL = "ted-sws"
+RUN_ENV_NAME = "__MAPPING_WORKBENCH_RUN_ENV__"
+RUN_ENV_VAL = "mapping-workbench"
 RUN_TEST_ENV_VAL = "test"
 os.environ[RUN_ENV_NAME] = RUN_ENV_VAL
