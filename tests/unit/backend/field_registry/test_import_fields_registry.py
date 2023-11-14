fe0df9cc0715ce5bd3edf2fd0f7ba240e63e4c53
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2023-11-14 23:33:42 +0200

    Update test_import_fields_registry.py

--- tests/unit/backend/field_registry/test_import_fields_registry.py
+++ tests/unit/backend/field_registry/test_import_fields_registry.py
@@ -3,18 +3,33 @@ from mapping_workbench.backend.fields_registry.services.import_fields_registry i
     import_fields_registry_from_eforms_fields
 
 
-def test_import_fields_registry_from_eforms_fields(eforms_fields):
-    assert eforms_fields
-    assert isinstance(eforms_fields, dict)
+def test_import_fields_registry_from_eforms_fields(eforms_fields_v191, eforms_fields_v180):
+    assert eforms_fields_v191
+    assert isinstance(eforms_fields_v191, dict)
 
-    field_registry = import_fields_registry_from_eforms_fields(eforms_fields_content=eforms_fields)
+    field_registry = import_fields_registry_from_eforms_fields(eforms_fields_content=eforms_fields_v191)
 
     assert field_registry
     assert isinstance(field_registry, FieldsRegistry)
-    assert field_registry.title == "eforms-sdk-1.10.0-SNAPSHOT"
+    assert field_registry.title == "eforms-sdk-1.9.1"
     assert field_registry.fields
     assert field_registry.nodes
     assert field_registry.root_node_id
     assert field_registry.root_node_id == "ND-Root"
-    assert len(field_registry.fields) == 1226
+    assert len(field_registry.fields) == 1224
     assert len(field_registry.nodes) == 286
+
+    assert eforms_fields_v180
+    assert isinstance(eforms_fields_v180, dict)
+
+    field_registry = import_fields_registry_from_eforms_fields(eforms_fields_content=eforms_fields_v180)
+
+    assert field_registry
+    assert isinstance(field_registry, FieldsRegistry)
+    assert field_registry.title == "eforms-sdk-1.8.0"
+    assert field_registry.fields
+    assert field_registry.nodes
+    assert field_registry.root_node_id
+    assert field_registry.root_node_id == "ND-Root"
+    assert len(field_registry.fields) == 771
+    assert len(field_registry.nodes) == 272
