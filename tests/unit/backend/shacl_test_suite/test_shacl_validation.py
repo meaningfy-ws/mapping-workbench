31167011bf5666bed3ecb0ec943e774a9499c75f
Dumitru <d.prijilevschi@gmail.com>
2023-11-17 15:23:04 +0300

    improve tests to cover validation results content

--- tests/unit/backend/shacl_test_suite/test_shacl_validation.py
+++ tests/unit/backend/shacl_test_suite/test_shacl_validation.py
@@ -3,6 +3,7 @@ from pydantic import ValidationError
 
 from mapping_workbench.backend.shacl_test_suite.adapters.validator import SHACLValidator
 from mapping_workbench.backend.shacl_test_suite.models.entity import SHACLTestSuite
+from mapping_workbench.backend.shacl_test_suite.models.validator import SHACLTestDataValidationResult
 from mapping_workbench.backend.shacl_test_suite.services.shacl_validator import \
     validate_test_data_with_shacl_test_suite, validate_tests_data_with_shacl_tests
 from mapping_workbench.backend.test_data_suite.models.entity import TestDataFileResource
@@ -40,6 +41,12 @@ async def test_validate_rdf_manifestation_with_shacl_test_suite(dummy_test_data_
     assert result.id is not None
     assert result.shacl_validation_result.id is not None
 
+    shacl_result: SHACLTestDataValidationResult = result.shacl_validation_result
+    assert shacl_result.identifier == dummy_test_data_file_resource.filename
+    assert shacl_result.conforms is not None
+    assert shacl_result.results_dict is not None
+    assert shacl_result.error is None
+
 
 @pytest.mark.asyncio
 async def test_validate_tests_data_with_shacl_tests(dummy_test_data_file_resource: TestDataFileResource,
@@ -52,12 +59,9 @@ async def test_validate_tests_data_with_shacl_tests(dummy_test_data_file_resourc
         assert result.id is not None
         assert result.shacl_validation_result.id is not None
 
+        shacl_result: SHACLTestDataValidationResult = result.shacl_validation_result
+        assert shacl_result.identifier == dummy_test_data_file_resource.filename
+        assert shacl_result.conforms is not None
+        assert shacl_result.results_dict is not None
+        assert shacl_result.error is None
 
-def test_shacl_validator(dummy_test_data_file_resource: TestDataFileResource, dummy_shacl_test_suite: SHACLTestSuite):
-    shacl_validator = SHACLValidator(test_data=dummy_test_data_file_resource)
-
-    assert shacl_validator.validate(shacl_files=dummy_shacl_test_suite.file_resources) is not None
-    assert shacl_validator.validate(None) is not None
-
-    with pytest.raises(ValidationError):
-        SHACLValidator(resource_id="None", rdf_manifestation="None")
--- tests/unit/backend/sparql_test_suite/test_sparql_validation.py
+++ tests/unit/backend/sparql_test_suite/test_sparql_validation.py
@@ -7,7 +7,8 @@ from mapping_workbench.backend.sparql_test_suite.services.sparql_validator impor
 from mapping_workbench.backend.test_data_suite.models.entity import TestDataFileResource
 
 
-def test_sparql_validator(dummy_test_data_file_resource: TestDataFileResource, dummy_sparql_test_suite: SPARQLTestSuite):
+def test_sparql_validator(dummy_test_data_file_resource: TestDataFileResource,
+                          dummy_sparql_test_suite: SPARQLTestSuite):
     sparql_validation_adapter = SPARQLValidator(dummy_test_data_file_resource)
     sparql_validator_result = sparql_validation_adapter.validate(sparql_queries=dummy_sparql_test_suite.file_resources)
 
@@ -15,23 +16,42 @@ def test_sparql_validator(dummy_test_data_file_resource: TestDataFileResource, d
     assert len(sparql_validator_result.ask_results) == len(dummy_sparql_test_suite.file_resources)
 
     with pytest.raises(ValueError):
-        sparql_validation_adapter = SPARQLValidator(None)
+        SPARQLValidator(None)
+
+    validator_query_results = sparql_validator_result.ask_results
+
+    for validator_result, sparql_query in zip(validator_query_results, dummy_sparql_test_suite.file_resources):
+        assert validator_result.query.content == sparql_query.content
+        assert validator_result.query_result == True
 
 
 @pytest.mark.asyncio
 async def test_validate_test_data_with_sparql_test_suite(dummy_test_data_file_resource: TestDataFileResource,
                                                          dummy_sparql_test_suite: SPARQLTestSuite):
-    result_test_data = await validate_test_data_with_sparql_test_suite(test_data=dummy_test_data_file_resource,
-                                                                       sparql_test_suite=dummy_sparql_test_suite)
+    test_data_results = await validate_test_data_with_sparql_test_suite(test_data=dummy_test_data_file_resource,
+                                                                        sparql_test_suite=dummy_sparql_test_suite)
 
-    assert result_test_data.sparql_validation_result is not None
+    assert test_data_results.sparql_validation_result is not None
+
+    for validator_result, sparql_query in zip(test_data_results.sparql_validation_result.ask_results,
+                                              dummy_sparql_test_suite.file_resources):
+        assert validator_result.query.content == sparql_query.content
+        assert validator_result.query_result == True
 
 
 @pytest.mark.asyncio
 async def test_validate_tests_data_with_shacl_tests(dummy_test_data_file_resource: TestDataFileResource,
                                                     dummy_sparql_test_suite: SPARQLTestSuite):
-    test_data_results = await validate_tests_data_with_shacl_tests(tests_data=[dummy_test_data_file_resource],
-                                                                  sparql_tests=dummy_sparql_test_suite.file_resources)
+    test_data_list = [dummy_test_data_file_resource]
+    test_data_results = await validate_tests_data_with_shacl_tests(tests_data=test_data_list,
+                                                                   sparql_tests=dummy_sparql_test_suite.file_resources)
 
     for result_test_data in test_data_results:
         assert result_test_data.sparql_validation_result is not None
+
+    for test_data_resource in test_data_results:
+
+        for query_result, sparql_query in zip(test_data_resource.sparql_validation_result.ask_results,
+                                              dummy_sparql_test_suite.file_resources):
+            assert query_result.query.content == sparql_query.content
+            assert query_result.query_result == True
