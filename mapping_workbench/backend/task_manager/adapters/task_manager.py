456e1065e354ed09644d0621594788ea1fc3e5fe
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2023-12-23 13:27:59 +0200

    add documentation

--- mapping_workbench/backend/task_manager/adapters/task_manager.py
+++ mapping_workbench/backend/task_manager/adapters/task_manager.py
@@ -1,3 +1,4 @@
+import concurrent
 from concurrent.futures import CancelledError
 from typing import List
 
@@ -7,6 +8,12 @@ from mapping_workbench.backend.task_manager.adapters.task import Task, TaskStatu
 
 
 def on_task_done_callback(future):
+    """
+    Callback function that is called when task is finished.
+    It updates task status, started_at, finished_at and exception_message.
+    :param future: future of the task that is finished (ProcessFuture)
+    :return: None
+    """
     task = future.task
     task.reset_future()
     try:
@@ -18,15 +25,9 @@ def on_task_done_callback(future):
     except CancelledError:
         task.update_task_status(TaskStatus.CANCELED)
         task.update_exception_message("Task was canceled!")
-    except OSError as error:
-        if len(error.args) > 0:
-            if error.args[0] == "Task timeout":
-                task.update_task_status(TaskStatus.TIMEOUT)
-                task.update_exception_message(f"Task took longer than {task.task_metadata.task_timeout} seconds")
-        else:
-            task.update_task_status(TaskStatus.FAILED)
-            task.update_exception_message(
-                f"Task raised error: {str(error)}\n{error.__traceback__}, error_type={type(error)}")
+    except (concurrent.futures.TimeoutError, TimeoutError) as error:
+        task.update_task_status(TaskStatus.TIMEOUT)
+        task.update_exception_message(f"Task took longer than {task.task_metadata.task_timeout} seconds")
     except Exception as error:
         task.update_task_status(TaskStatus.FAILED)
         task.update_exception_message(
@@ -34,13 +35,32 @@ def on_task_done_callback(future):
 
 
 class TaskManager:
+    """
+    TaskManager is a class that manages tasks.
+    It is responsible for:
+    - adding tasks
+    - canceling tasks
+    - deleting tasks
+    - deleting all tasks
+    - getting task statuses
+    - getting active tasks
+    - checking if there are any active tasks
+    - stopping task manager
+    """
 
     def __init__(self, max_workers: int):
+        """
+        :param max_workers: number of workers in the pool
+        """
         self.workers_pool = ProcessPool(max_workers=max_workers)
-        self.future_queue = []
         self.tasks = []
 
     def add_task(self, task: Task):
+        """
+        Adds task to the task manager
+        :param task: task to be added
+        :return: None
+        """
         future: ProcessFuture = self.workers_pool.schedule(task.task_function, args=list(task.task_args),
                                                            kwargs=task.task_kwargs,
                                                            timeout=task.task_metadata.task_timeout)
@@ -50,9 +70,17 @@ class TaskManager:
         self.tasks.append(task)
 
     def get_active_tasks(self) -> List[Task]:
+        """
+        Returns list of active tasks
+        :return: list of active tasks
+        """
         return [task for task in self.tasks if task.get_future() is not None]
 
     def update_task_statuses(self):
+        """
+        Updates task statuses
+        :return: None
+        """
         for task in self.tasks:
             future = task.get_future()
             if future:
@@ -60,16 +88,31 @@ class TaskManager:
                     task.update_task_status(TaskStatus.RUNNING)
 
     def get_task_statuses(self, update_task_statuses: bool = True) -> List[TaskMetadata]:
+        """
+        Returns list of task metadata
+        :param update_task_statuses: if True, task statuses will be updated before returning
+        :return: list of task metadata
+        """
         if update_task_statuses:
             self.update_task_statuses()
         return [task.get_task_metadata() for task in self.tasks]
 
     def cancel_task(self, task_id: str):
+        """
+        Cancels task with given task_id
+        :param task_id: task id of the task to be canceled
+        :return: None
+        """
         for task in self.tasks:
             if task.get_task_id() == task_id:
                 task.cancel()
 
     def delete_task(self, task_id: str):
+        """
+        Deletes task with given task_id
+        :param task_id: task id of the task to be deleted
+        :return: None
+        """
         for task in self.tasks:
             if task.get_task_id() == task_id:
                 task.cancel()
@@ -77,17 +120,31 @@ class TaskManager:
                 break
 
     def delete_all_tasks(self):
+        """
+        Deletes all tasks from the task manager and cancels them if they are active
+        :return: None
+        """
         for task in self.tasks:
             task.cancel()
         self.tasks = []
 
     def is_active(self) -> bool:
+        """
+        Checks if there are any active tasks
+        :return: True if there are any active tasks, False otherwise
+        """
         return len(self.get_active_tasks()) > 0
 
     def stop(self):
+        """
+        Stops task manager and closes workers pool and joins it to the main thread (waits for all workers to finish).
+        """
         self.workers_pool.close()
         self.workers_pool.join()
 
     def __del__(self):
+        """
+        Stops task manager and closes workers pool and joins it to the main thread (waits for all workers to finish).
+        """
         self.workers_pool.close()
         self.workers_pool.join()
