9c3e86b6a20199d8f33d67436d6c4ab54aad324c
Victor Frunze <silverberserk@gmail.com>
2024-01-31 16:34:27 +0200

    add route and get_validation_results

--- mapping_workbench/backend/package_exporter/services/export_mapping_suite_v3.py
+++ mapping_workbench/backend/package_exporter/services/export_mapping_suite_v3.py
@@ -66,6 +66,31 @@ async def export_specific_package_state(mapping_package: MappingPackage,
     )
 
 
+async def get_validation_reports(mapping_package: MappingPackage,
+                                 mapping_package_state_id: PydanticObjectId) -> str:
+
+    """
+
+    :param mapping_package:
+    :param mapping_package_state_id:
+    :return:
+    """
+
+    mapping_package_state: MappingPackageState = await get_specific_mapping_package_state(mapping_package_state_id)
+
+    if not mapping_package_state:
+        raise ResourceNotFoundException()
+
+    project = await get_project(mapping_package.project.to_ref().id)
+
+    exporter: PackageStateExporter = PackageStateExporter(
+        package_state=mapping_package_state,
+        project=project
+    )
+
+    return await exporter.get_validation_reports()
+
+
 async def export_package_state(mapping_package_state: MappingPackageState, project: Project) -> bytes:
     exporter: PackageStateExporter = PackageStateExporter(
         package_state=mapping_package_state,
