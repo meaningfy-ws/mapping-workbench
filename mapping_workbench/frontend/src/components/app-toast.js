df7925293179b7ad9bd71909aa11b60ca3c195cd
Victor Frunze <silverberserk@gmail.com>
2024-04-02 11:48:16 +0300

    MWB-362: add toast with close button

---
+++ mapping_workbench/frontend/src/components/app-toast.js
@@ -0,0 +1,27 @@
+import toast from "react-hot-toast";
+
+import Stack from "@mui/material/Stack";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/HighlightOff";
+
+const toastClose = (content, id) => (
+        <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="center">
+            {content}
+            {id && <IconButton onClick={() => toast.dismiss(id)}><CloseIcon/></IconButton>}
+        </Stack>
+    )
+
+export const toastLoad = (content, id) => (
+    toast.loading(toastClose(content, id), {id})
+)
+
+export const toastError = (content, id) => (
+    toast.error(toastClose(content, id), {id, duration: 60000})
+)
+
+export const toastSuccess = (content, id) => (
+    toast.success(toastClose(content, id), {id, duration: 60000})
+)
