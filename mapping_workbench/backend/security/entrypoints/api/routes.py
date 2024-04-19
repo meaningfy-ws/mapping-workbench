c881f57bcce8078318f9f400b51a6fdcf888f7a6
Kolea PLESCO <kaleanych@UNIVERSE.local>
2024-04-19 20:17:30 +0300

    WIP

--- mapping_workbench/backend/security/entrypoints/api/routes.py
+++ mapping_workbench/backend/security/entrypoints/api/routes.py
@@ -29,11 +29,6 @@ router.include_router(
 )
 
 
-@router.get("/keep-alive")
-async def route_keep_alive(user: User = Depends(current_active_user)):
-    return {"message": f"Hello {user.email}!"}
-
-
 @router.get("/authenticated-route")
 async def authenticated_route(user: User = Depends(current_active_user)):
     return {"message": f"Hello {user.email}!"}
