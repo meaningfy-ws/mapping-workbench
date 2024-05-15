f63502e330215c162464e6c231043b7432629c1a
Victor Frunze <silverberserk@gmail.com>
2024-05-15 09:20:07 +0300

    MWB-505: make switch work

--- mapping_workbench/frontend/src/layouts/app/time-switch/time-switch.js
+++ mapping_workbench/frontend/src/layouts/app/time-switch/time-switch.js
@@ -1,22 +1,31 @@
-import {usePopover} from "../../../hooks/use-popover";
-
 import IconButton from '@mui/material/IconButton';
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import Tooltip from '@mui/material/Tooltip';
 import Popover from "@mui/material/Popover";
 import MenuItem from "@mui/material/MenuItem";
+import Typography from "@mui/material/Typography";
+
+import {useGlobalState} from "../../../hooks/use-global-state";
+import {usePopover} from "../../../hooks/use-popover";
 const TimeSwitch = () => {
 
-  const popover = usePopover();
+    const popover = usePopover();
+    const globalState = useGlobalState()
+    const isActive = globalState.timeSetting === 'luxembourg'
 
-  return (
+    const handleTimeUpdate = (time) => {
+        popover.handleClose()
+        globalState.handleGlobalStateUpdate('timeSetting', time)
+    }
+
+    return (
     <>
       <Tooltip title="Language">
         <IconButton
           onClick={popover.handleOpen}
           ref={popover.anchorRef}
         >
-            <AccessAlarmIcon/>
+            <AccessAlarmIcon color={isActive ? 'primary' :'' }/>
         </IconButton>
       </Tooltip>
       <Popover open={popover.open}
@@ -33,11 +42,11 @@ const TimeSwitch = () => {
                }}
                PaperProps={{ sx: { width: 220 } }}
       >
-        <MenuItem>Luxemburg Time</MenuItem>
-        <MenuItem>Local Time</MenuItem>
+        <MenuItem onClick={() => handleTimeUpdate('luxembourg')}><Typography color={isActive && 'primary'}>Luxembourg Time</Typography></MenuItem>
+          <MenuItem onClick={() => handleTimeUpdate('local')}><Typography color={!isActive && 'primary'}>Local Time</Typography></MenuItem>
       </Popover>
     </>
-  );
+    );
 };
 
 export default TimeSwitch
