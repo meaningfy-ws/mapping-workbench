42ea16b33ef65a3b232dd3af7d630c96d63409cf
Dumitru <d.prijilevschi@gmail.com>
2024-05-20 00:45:36 +0300

    Send internal errors if the environment is not prod

--- mapping_workbench/backend/core/entrypoints/api/main.py
+++ mapping_workbench/backend/core/entrypoints/api/main.py
@@ -1,22 +1,24 @@
-from fastapi import FastAPI, APIRouter, Depends
+from fastapi import APIRouter, Depends
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from httpx_oauth.clients.google import GoogleOAuth2
 
-from mapping_workbench.backend.fields_registry.entrypoints.api import routes as fields_registry
 from mapping_workbench.backend.conceptual_mapping_rule.entrypoints.api import routes as conceptual_mapping_rule_routes
 from mapping_workbench.backend.config import settings
 from mapping_workbench.backend.config.entrypoints.api import routes as config_routes
 from mapping_workbench.backend.core.entrypoints.api import routes as core_routes
 from mapping_workbench.backend.core.services.project_initilisers import init_project_models
 from mapping_workbench.backend.database.adapters.mongodb import DB
+from mapping_workbench.backend.fields_registry.entrypoints.api import routes as fields_registry
 from mapping_workbench.backend.mapping_package.entrypoints.api import routes as mapping_package_routes
-from mapping_workbench.backend.package_importer.entrypoints.api import routes as package_importer_routes
-from mapping_workbench.backend.package_exporter.entrypoints.api import routes as package_exporter_routes
-from mapping_workbench.backend.package_processor.entrypoints.api import routes as package_processor_routes
-from mapping_workbench.backend.package_validator.entrypoints.api import routes as package_validator_routes
 from mapping_workbench.backend.mapping_rule_registry.entrypoints.api import routes as mapping_rule_registry_routes
 from mapping_workbench.backend.ontology.entrypoints.api import routes as ontology_routes
 from mapping_workbench.backend.ontology_file_collection.entrypoints.api import routes as ontology_file_collection_routes
+from mapping_workbench.backend.package_exporter.entrypoints.api import routes as package_exporter_routes
+from mapping_workbench.backend.package_importer.entrypoints.api import routes as package_importer_routes
+from mapping_workbench.backend.package_processor.entrypoints.api import routes as package_processor_routes
+from mapping_workbench.backend.package_validator.entrypoints.api import routes as package_validator_routes
 from mapping_workbench.backend.project.entrypoints.api import routes as project_routes
 from mapping_workbench.backend.resource_collection.entrypoints.api import routes as resource_collection_routes
 from mapping_workbench.backend.security.entrypoints.api import routes as security_routes
@@ -59,6 +61,15 @@ app.add_middleware(
 @app.on_event("startup")
 async def on_startup():
     await init_project_models(mongodb_database=DB.get_database())
+
+
+if settings.ENVIRONMENT != "prod":
+    @app.exception_handler(Exception)
+    async def all_exception_handler(request: Request, exception: Exception):
+        return JSONResponse(
+            status_code=422,
+            content={"detail": [{"msg": f"Exception name: {exception.__class__.__name__} Error: {str(exception)}"}]},
+        )
 
 app_router = APIRouter()
 
