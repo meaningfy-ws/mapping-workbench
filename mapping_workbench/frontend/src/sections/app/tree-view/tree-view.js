7483c68b4a2c7e63dd6aebfff8793edf76265704
Victor Frunze <silverberserk@gmail.com>
2024-05-31 10:46:19 +0300

    MWB-558: fixing styles

--- mapping_workbench/frontend/src/sections/app/tree-view/tree-view.js
+++ mapping_workbench/frontend/src/sections/app/tree-view/tree-view.js
@@ -1,32 +1,16 @@
-import {useEffect, useState, forwardRef} from "react";
-import * as PropTypes from "prop-types";
+import {useEffect, useState} from "react";
 
 import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView';
-import {
-    TreeItem2Checkbox, TreeItem2Content, TreeItem2GroupTransition,
-    TreeItem2Icon,
-    TreeItem2IconContainer, TreeItem2Label,
-    TreeItem2Provider,
-    TreeItem2Root
-} from "@mui/x-tree-view";
+
 import FolderIcon from '@mui/icons-material/Folder';
 import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
-import Icon from "@mui/material/Icon";
-import {unstable_useTreeItem2 as useTreeItem2} from "@mui/x-tree-view/useTreeItem2";
-import Box from "@mui/system/Box";
-import {styled} from "@mui/material/styles";
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-function CustomTreeItemContent(props) {
-    return null;
-}
+import {TreeItem, treeItemClasses} from "@mui/x-tree-view";
+import Stack from "@mui/material/Stack";
 
-CustomTreeItemContent.propTypes = {
-    onClick: PropTypes.func,
-    ref: PropTypes.func,
-    onMouseDown: PropTypes.func,
-    status: PropTypes.any,
-    children: PropTypes.node
-};
+
 const TreeView = (props) => {
     const {sectionApi} = props;
 
@@ -36,42 +20,6 @@ const TreeView = (props) => {
         getTheTree()
     }, []);
 
-const CustomTreeItemContent = styled(TreeItem2Content)(({ theme }) => ({
-  padding: theme.spacing(1, 1),
-}));
-
-// eslint-disable-next-line react/display-name
-const CustomTreeItem = forwardRef((props, ref) => {
-  const { id, itemId, label, disabled, children, ...other } = props;
-
-  const {
-    getRootProps,
-    getContentProps,
-    getIconContainerProps,
-    getCheckboxProps,
-    getLabelProps,
-    getGroupTransitionProps,
-    status,
-  } = useTreeItem2({ id, itemId, children, label, disabled, rootRef: ref });
-
-  return (
-    <TreeItem2Provider itemId={itemId}>
-      <TreeItem2Root {...getRootProps(other)}>
-        <CustomTreeItemContent {...getContentProps()}>
-          <TreeItem2IconContainer {...getIconContainerProps()}>
-            <TreeItem2Icon status={status} />
-          </TreeItem2IconContainer>
-          <TreeItem2Checkbox {...getCheckboxProps()} />
-          <Box sx={{ flexGrow: 1, display: 'flex', gap: 1 }}>
-            {children ? <FolderIcon/> : <InsertDriveFileIcon/>}
-            <TreeItem2Label {...getLabelProps()} />
-          </Box>
-        </CustomTreeItemContent>
-        {children && <TreeItem2GroupTransition {...getGroupTransitionProps()} />}
-      </TreeItem2Root>
-    </TreeItem2Provider>
-  );
-});
 
     const getTheTree = () => {
         sectionApi.getItemsTree()
@@ -81,18 +29,27 @@ const CustomTreeItem = forwardRef((props, ref) => {
 
     const treeMenu = (item) => {
         return (
-            <CustomTreeItem key={item.id}
-                      slots={{icon:<Icon><FolderIcon/></Icon>}}
+            <TreeItem key={item.id}
                       itemId={item.id}
                       label={item.label}
                         onClick={(e)=>console.log(e)}>
                 {item.children?.map(e => treeMenu(e))}
-            </CustomTreeItem>
+            </TreeItem>
         )
     }
 
+    const FileIcon = () => <InsertDriveFileIcon />
+    const CollapseIcon = () => <Stack direction='row'><ExpandMoreIcon/><FolderIcon/></Stack>
+    const ExpandIcon = () => <Stack direction='row'><ChevronRightIcon/><FolderIcon/></Stack>
+
     return(
-        <SimpleTreeView>
+        <SimpleTreeView slots={{
+            expandIcon: ExpandIcon,
+            collapseIcon: CollapseIcon,
+            endIcon: FileIcon
+        }}
+         sx={{ overflowX: 'hidden', minHeight: 270, flexGrow: 1,
+             [`& .${treeItemClasses.iconContainer}`]: { width: 40, color: 'gray'} }}>
             {state?.map(treeMenu)}
         </SimpleTreeView>
     )
