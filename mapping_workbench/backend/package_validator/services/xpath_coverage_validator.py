185af3df43501afad95fa26c2c75a2cd8e3da4d8
Kolea PLESCO <kaleanych@UNIVERSE.local>
2024-07-10 19:16:55 +0200

    fix-asyncio-task-run

--- mapping_workbench/backend/package_validator/services/xpath_coverage_validator.py
+++ mapping_workbench/backend/package_validator/services/xpath_coverage_validator.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from mapping_workbench.backend.logger.services import mwb_logger
 from mapping_workbench.backend.mapping_package.models.entity import MappingPackageState
 from mapping_workbench.backend.package_validator.adapters.xpath_validator import XPATHValidator
 from mapping_workbench.backend.package_validator.models.xpath_validation import XPathAssertion, \
@@ -99,54 +100,59 @@ def update_xpath_assertion_test_data_entry_xpaths(
 
 
 def compute_xpath_assertions_for_mapping_package(mapping_package_state: MappingPackageState):
-    test_data_suites: List[TestDataSuiteState] = mapping_package_state.test_data_suites
+    try:
+        test_data_suites: List[TestDataSuiteState] = mapping_package_state.test_data_suites
 
-    conceptual_mapping_rule_states = mapping_package_state.conceptual_mapping_rules
-    for test_data_suite in test_data_suites:
-        test_data_states: List[TestDataState] = test_data_suite.test_data_states
-        for test_data_state in test_data_states:
-            xml_content = test_data_state.xml_manifestation.content
-            xpath_validator: XPATHValidator = XPATHValidator(xml_content)
+        conceptual_mapping_rule_states = mapping_package_state.conceptual_mapping_rules
+        for test_data_suite in test_data_suites:
+            test_data_states: List[TestDataState] = test_data_suite.test_data_states
+            for test_data_state in test_data_states:
+                xml_content = test_data_state.xml_manifestation.content
+                mwb_logger.log_all_info("Validating XPATH for TEST_DATA :: " + test_data_state.title)
+                xpath_validator: XPATHValidator = XPATHValidator(xml_content)
 
-            for conceptual_mapping_rule_state in conceptual_mapping_rule_states:
-                structural_element = conceptual_mapping_rule_state.source_structural_element
-                if structural_element:
-                    cm_xpath = structural_element.absolute_xpath
-                    cm_sdk_id = structural_element.sdk_element_id
-                    cm_sdk_title = structural_element.name
+                for conceptual_mapping_rule_state in conceptual_mapping_rule_states:
+                    structural_element = conceptual_mapping_rule_state.source_structural_element
+                    if structural_element:
+                        cm_xpath = structural_element.absolute_xpath
+                        cm_sdk_id = structural_element.sdk_element_id
+                        cm_sdk_title = structural_element.name
 
-                    validation_message = None
-                    xpaths: List[XPathAssertionEntry] = []
-                    try:
-                        xpaths = xpath_validator.validate(cm_xpath)
-                    except Exception as e:
-                        validation_message = str(e)
+                        validation_message = None
+                        xpaths: List[XPathAssertionEntry] = []
+                        try:
+                            mwb_logger.log_all_info("Validating XPATH :: " + cm_xpath)
+                            xpaths = xpath_validator.validate(cm_xpath)
+                        except Exception as e:
+                            validation_message = str(e)
 
-                    update_xpath_assertion(
-                        state=mapping_package_state,
-                        element_id=cm_sdk_id,
-                        xpath=cm_xpath,
-                        element_title=cm_sdk_title,
-                        test_data_suite=test_data_suite,
-                        test_data_state=test_data_state,
-                        xpaths=xpaths
-                    )
-                    update_xpath_assertion(
-                        state=test_data_suite,
-                        element_id=cm_sdk_id,
-                        xpath=cm_xpath,
-                        element_title=cm_sdk_title,
-                        test_data_suite=test_data_suite,
-                        test_data_state=test_data_state,
-                        xpaths=xpaths
-                    )
-                    update_xpath_assertion(
-                        state=test_data_state,
-                        element_id=cm_sdk_id,
-                        xpath=cm_xpath,
-                        element_title=cm_sdk_title,
-                        test_data_suite=test_data_suite,
-                        test_data_state=test_data_state,
-                        xpaths=xpaths,
-                        validation_message=validation_message
-                    )
+                        update_xpath_assertion(
+                            state=mapping_package_state,
+                            element_id=cm_sdk_id,
+                            xpath=cm_xpath,
+                            element_title=cm_sdk_title,
+                            test_data_suite=test_data_suite,
+                            test_data_state=test_data_state,
+                            xpaths=xpaths
+                        )
+                        update_xpath_assertion(
+                            state=test_data_suite,
+                            element_id=cm_sdk_id,
+                            xpath=cm_xpath,
+                            element_title=cm_sdk_title,
+                            test_data_suite=test_data_suite,
+                            test_data_state=test_data_state,
+                            xpaths=xpaths
+                        )
+                        update_xpath_assertion(
+                            state=test_data_state,
+                            element_id=cm_sdk_id,
+                            xpath=cm_xpath,
+                            element_title=cm_sdk_title,
+                            test_data_suite=test_data_suite,
+                            test_data_state=test_data_state,
+                            xpaths=xpaths,
+                            validation_message=validation_message
+                        )
+    except Exception as e:
+        raise e
