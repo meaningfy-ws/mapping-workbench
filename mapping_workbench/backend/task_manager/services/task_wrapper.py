4135c2298297197fc1c59dc1d2b04fe51269b0e6
Kolea PLESCO <kaleanych@UNIVERSE.local>
2024-07-11 18:20:55 +0200

    fix-asyncio-task-run

--- mapping_workbench/backend/task_manager/services/task_wrapper.py
+++ mapping_workbench/backend/task_manager/services/task_wrapper.py
@@ -1,10 +1,21 @@
 import asyncio
+from contextlib import contextmanager
 
 from mapping_workbench.backend.config import settings
 from mapping_workbench.backend.core.services.project_initilisers import init_project_models
 from mapping_workbench.backend.database.adapters.mongodb import DB
 from mapping_workbench.backend.task_manager.adapters.task import Task, TaskMetadata
 from mapping_workbench.backend.task_manager.entrypoints import AppTaskManager
+
+
+# @contextmanager
+def task_event_loop():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        yield loop
+    finally:
+        loop.close()
 
 
 async def init_task():
@@ -16,15 +27,8 @@ def run_task(task_to_run, *args):
         await init_task()  # This is because of beanie implementation
         await task_to_run(*args)
 
-    loop = asyncio.new_event_loop()
-    # asyncio.set_event_loop(loop)
-
-    # try:
-    #     loop.run_until_complete(task())
-    # finally:
-    #     loop.close()
-
-    loop.run_until_complete(task())
+    with task_event_loop() as loop:
+        loop.run_until_complete(task())
 
 
 def add_task(task_to_run, task_name, task_timeout, created_by, *args) -> TaskMetadata:
