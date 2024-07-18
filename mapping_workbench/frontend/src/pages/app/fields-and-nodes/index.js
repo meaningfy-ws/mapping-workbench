36fc00dfdcfb1315b7764adfbee3e198d311804c
Victor Frunze <silverberserk@gmail.com>
2024-07-18 10:00:50 +0300

    MWB-669: add highlighter

---
+++ mapping_workbench/frontend/src/pages/app/fields-and-nodes/index.js
@@ -0,0 +1,83 @@
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Card from '@mui/material/Card';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+import {paths} from 'src/paths';
+import {Seo} from 'src/components/seo';
+import {Layout as AppLayout} from 'src/layouts/app';
+import {mappingPackagesApi as sectionApi} from 'src/api/mapping-packages';
+import {RouterLink} from 'src/components/router-link';
+import {BreadcrumbsSeparator} from 'src/components/breadcrumbs-separator';
+import XMLData from 'cn_81'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+
+const Page = () => {
+
+    return (
+        <>
+            <Seo title={`App: ${sectionApi.SECTION_TITLE} List`}/>
+            <Stack spacing={4}>
+                <Stack
+                    direction="row"
+                    justifyContent="space-between"
+                    spacing={4}
+                >
+                    <Stack spacing={1}>
+                        <Typography variant="h4">
+                            {sectionApi.SECTION_TITLE}
+                        </Typography>
+                        <Breadcrumbs separator={<BreadcrumbsSeparator/>}>
+                            <Link
+                                color="text.primary"
+                                component={RouterLink}
+                                href={paths.index}
+                                variant="subtitle2"
+                            >
+                                App
+                            </Link>
+                            <Link
+                                color="text.primary"
+                                component={RouterLink}
+                                href={paths.app[sectionApi.section].index}
+                                variant="subtitle2"
+                            >
+                                {sectionApi.SECTION_TITLE}
+                            </Link>
+                            <Typography
+                                color="text.secondary"
+                                variant="subtitle2"
+                            >
+                                List
+                            </Typography>
+                        </Breadcrumbs>
+                    </Stack>
+                    <Stack
+                        alignItems="center"
+                        direction="row"
+                        spacing={3}
+                    >
+                    </Stack>
+
+                </Stack>
+                <Card>
+                    <SyntaxHighlighter
+                        language="xml"
+                        wrapLines
+                        lineProps={{ style: { wordBreak: 'break-all', whiteSpace: 'pre-wrap' } }}>
+                        {XMLData}
+                    </SyntaxHighlighter>
+                </Card>
+            </Stack>
+        </>
+    );
+};
+
+Page.getLayout = (page) => (
+    <AppLayout>
+        {page}
+    </AppLayout>
+);
+
+export default Page;
