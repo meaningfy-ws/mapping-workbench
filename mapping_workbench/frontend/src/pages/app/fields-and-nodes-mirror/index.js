000def236609c01889d3cac0433987041109490e
Victor Frunze <silverberserk@gmail.com>
2024-07-22 15:44:01 +0300

    MWB-669: use codemirror add actions

--- mapping_workbench/frontend/src/pages/app/fields-and-nodes-mirror/index.js
+++ mapping_workbench/frontend/src/pages/app/fields-and-nodes-mirror/index.js
@@ -7,12 +7,11 @@ import Typography from '@mui/material/Typography';
 import {paths} from 'src/paths';
 import {Seo} from 'src/components/seo';
 import {Layout as AppLayout} from 'src/layouts/app';
-import {mappingPackagesApi as sectionApi} from 'src/api/mapping-packages';
 import {RouterLink} from 'src/components/router-link';
 import {BreadcrumbsSeparator} from 'src/components/breadcrumbs-separator';
 import XMLData from 'cn_81'
-import {UnControlled as CodeMirror} from 'react-codemirror2'
-import {useEffect, useState} from "react";
+import {Controlled as CodeMirror} from 'react-codemirror2'
+import {useEffect, useRef, useState} from "react";
 import Input from "@mui/material/Input";
 import Button from "@mui/material/Button";
 import 'codemirror/lib/codemirror.css';
@@ -25,6 +24,7 @@ import 'codemirror/theme/material.css';
 
 const Page = () => {
 
+    const codeMirrorRef = useRef()
     // const language = () => [import('codemirror/mode/xml/xml')];
 
     const [load,setLoad] = useState(false)
@@ -32,6 +32,7 @@ const Page = () => {
     useEffect(() => {
         dinamycImport()
             .then(() => setLoad(true))
+        // const current = codeMirrorRef.current?.editor.display.wrapper.style.height = "1000px";
     }, []);
 
 
@@ -53,26 +54,84 @@ const Page = () => {
         // return node.value
     }
 
-    const renderRow = (rows, css, highlight) => {
-        return rows.map((node, i) => {
-            const nodeCss = Object.assign({}, ...node.properties?.className.map(e=>css[e]).filter(e => e) ?? [])
-            const isTagNode = ['token','tag'].every(e =>node.properties?.className.includes(e))
-                && !['punctuation','attr-name','attr-value'].some(e => node.properties?.className.includes(e))
-                && node.children?.[0]?.value !== ' '
-            return <span key={node.properties?.key ?? i}
-                         className={node.properties?.className?.join(' ')}
-                         // onClick={node.properties?.onClick}
-                         style={  {...nodeCss, ...node.properties?.style, backgroundColor: isTagNode && highlight ? 'yellow' : ''}}
-                    >
-                        {node.children ? renderRow(node.children, css, false) : node.value}
-                    </span>
-        });
+
+    const highlightLines = (editor, start, end) => {
+      const from={line: start, ch: 0};
+      const to= {line: end, ch: 10};
+      editor.markText(from, to, {className: "codemirror-highlighted"});
     }
 
+     const handleLineClick = (cm, lineNumber) => {
+        console.log(cm)
+        const doc = cm.getDoc();
+        const line = doc.getLine(lineNumber);
+        console.log('line',line)
+        const element = doc.markText({line: lineNumber, ch: 0}, {line: lineNumber, ch: line.length});
+        const xmlElement = new DOMParser().parseFromString(line, "text/xml").documentElement;
+
+        // console.log('handleLineClick',cm,lineNumber)
+        // const line = cm.getLine(lineNumber);
+        // alert(`Line ${lineNumber + 1} clicked: ${line}`);
+        if (xmlElement) {
+              const xpath = getXPath(xmlElement);
+              console.log(xpath)
+          }
+        };
+
+
+     const getElementByLineNumber = (element, lineNumber) => {
+        let currentLine = 1;
+        const traverse = (node) => {
+          if (node.nodeType === Node.ELEMENT_NODE || node.nodeType === Node.TEXT_NODE) {
+            if (currentLine === lineNumber) {
+              return node;
+            }
+            currentLine++;
+          }
+
+          let result = null;
+          for (let i = 0; i < node.childNodes.length; i++) {
+            result = traverse(node.childNodes[i]);
+            if (result) return result;
+          }
+          return null;
+    };
+
+
+     const onMount = (editor) => {
+        editor.on('gutterClick', (cm, lineNumber) => handleLineClick(cm, lineNumber));
+        editor.on('hover')
+     };
+
+
+     // const lines = [];
+     // const xpathsList = [];
+     // XMLData.forEach((row, index) => {
+     //    if (index > 0) { // assuming the first row is headers
+     //      lines.push(row[0]); // assuming your content is in the first column
+     //      xpathsList.push(row[1]); // assuming your XPaths are in the second column
+     //    }
+     // });
+
+
+    const getXPath = (element, path = []) => {
+        console.log(element,path,element.nodeType)
+        if (element.nodeType === Node.DOCUMENT_NODE) {
+          return '/' + path.join('/');
+        }
+
+        const { nodeName, parentNode } = element;
+        console.log('nodeName:',nodeName,'parentNode:',parentNode)
+        const siblings = Array.from(parentNode.childNodes).filter(sibling => sibling.nodeName === nodeName);
+        const index = siblings.indexOf(element) + 1;
+        path.unshift(`${nodeName}[${index}]`);
+
+        return getXPath(parentNode, path);
+      };
 
     return (
         <>
-            <Seo title={`App: ${sectionApi.SECTION_TITLE} List`}/>
+            <Seo title={`App: ${''} List`}/>
             <Stack spacing={4}>
                 <Stack
                     direction="row"
@@ -81,7 +140,7 @@ const Page = () => {
                 >
                     <Stack spacing={1}>
                         <Typography variant="h4">
-                            {sectionApi.SECTION_TITLE}
+                            {}
                         </Typography>
                         <Breadcrumbs separator={<BreadcrumbsSeparator/>}>
                             <Link
@@ -95,10 +154,10 @@ const Page = () => {
                             <Link
                                 color="text.primary"
                                 component={RouterLink}
-                                href={paths.app[sectionApi.section].index}
+                                href={'#'}
                                 variant="subtitle2"
                             >
-                                {sectionApi.SECTION_TITLE}
+                                {/*{sectionApi.SECTION_TITLE}*/}
                             </Link>
                             <Typography
                                 color="text.secondary"
@@ -118,14 +177,22 @@ const Page = () => {
                 </Stack>
                 <Card>
                     {load &&
-                   <CodeMirror
-                      value={XMLData}
-                      options={{
-                        mode: 'xml',
-                        theme: 'material',
-                        lineNumbers: true,
-                          disabled:true,
-                      }}
+                    <CodeMirror
+                       style={{"height": "800px"}}
+                       ref={codeMirrorRef}
+                       value={XMLData}
+                       options={{
+                          mode: 'xml',
+                          theme: 'material',
+                          lineNumbers: true,
+                          readOnly: true,
+                       }}
+                       // editorDidMount={editor => highlightLines(editor, 1, 4)}
+                      // onFocus={e}
+                      //  editorDidMount={onMount}
+                        onGutterClick={(cm, lineNumber) => handleLineClick(cm, lineNumber)}
+                       onRenderLine={(editor,line,element)=>console.log(editor,line,element)}
+                       onFocus={e => console.log(e)}
                     />}
                 </Card>
                 <Input value={nodeValue}/><Button onClick={() => setListOfNodes(e=>([...e, nodeValue]))}>Save</Button>
