e1c32b9669b3dcc5b75853b64407cb2c135e5cbe
Victor Frunze <silverberserk@gmail.com>
2024-08-06 12:59:39 +0300

    MWB-669: add styles

--- mapping_workbench/frontend/src/sections/app/fields-and-nodes/file.js
+++ mapping_workbench/frontend/src/sections/app/fields-and-nodes/file.js
@@ -1,33 +1,41 @@
 import {useEffect, useState} from "react";
 import {parseString} from "xml2js";
-import SaxonJS from 'saxon-js'
 
 import styles from './styles/style.module.scss'
 import Alert from "@mui/material/Alert";
+import classNames from "classnames/bind";
+import CircularProgress from "@mui/material/CircularProgress";
 
     const MARGIN = 14
     const ATTRIBUTE_SIGN = '$'
     const NAME_SIGH = '_'
 
+    const cx = classNames.bind(styles);
+
     const Attribute = ({ name, value, parent }) => {
         return (
             <span onClick={() => console.log([...parent, `[${name}='${value}']`].join('/'))}
-                  className={styles.attr}>
-                <span className={styles['attr-name']}>{` ${name}=`}</span>
+                  className={cx('attr')}>
+                <span className={cx('attr-name')}>{` ${name}=`}</span>
                 <span>{'"'}</span>
-                    <span className={styles['attr-value']}>{value}</span>
+                    <span className={cx('attr-value')}>{value}</span>
                 <span>{'"'}</span>
             </span>)
     }
 
 
-    const Tag = ({name, attributes, children, parent, level, isField}) => {
-       return (
+    const Tag = ({name, attributes, children, parent, level, isField, xpaths}) => {
+        const nodeXPath = [...parent, name].join('/')
+        const selectedNode = xpaths?.some(xpath => nodeXPath.endsWith(xpath))
+        return (
            <span style={{marginLeft: level * MARGIN}}
-                       className={styles['text-color']}>
+                       className={cx('text-color')}>
                <span>{'<'}</span>
                <span name='tag'
-                     className={styles.tag}
+                   className={cx('tag', {
+                       'tag-selected-field': (selectedNode && isField),
+                       'tag-selected-node' : (selectedNode && !isField)
+                   })}
                      onClick={() => console.log('parent', [...parent, name].join('/'))}>
                    {name}
                </span>
@@ -40,28 +48,29 @@ import Alert from "@mui/material/Alert";
                <span>{'>'}</span>
                {!isField && <br/>}
                {children}
-               {!isField && <br/>}
            <span style={{marginLeft: isField ? 0 : level * MARGIN}} >{'</'}</span>
            <span name='close-tag'
-                 className={styles.tag}
+                 className={cx('tag', {
+                       'tag-selected-field': (selectedNode && isField),
+                       'tag-selected-node' : (selectedNode && !isField)
+                   })}
                  onClick={() => console.log('parent', [...parent, name].join('/'))}>
                {name}
            </span>
            <span>{'>'}</span>
                <br/>
-               {/*{!isField && <br/>}*/}
         </span>)
     }
 
 
-    const BuildObject = ({nodes, level, parent}) => {
+    const BuildObject = ({nodes, level, parent, xpaths}) => {
         return nodes.map((e) => {
                 const [name, value] = e
-                // console.log('name=>', name, 'value=>', value, typeof value)
                 if(['0', '1', '2', '3'].includes(name))
                     return <BuildObject nodes={Object.entries(value).filter(en => en[0] !== ATTRIBUTE_SIGN)}
                                         key={'obj'+name}
                                         level={level}
+                                        xpaths={xpaths}
                                         parent={[...parent, name]}/>
                 if (typeof value == "string")
                     if(name !== NAME_SIGH)
@@ -69,23 +78,19 @@ import Alert from "@mui/material/Alert";
                                     name={name}
                                     isField
                                     parent={parent}
+                                    xpaths={xpaths}
                                     level={level}>
                                 <>
-                                    {/*<span style={{marginLeft: level*MARGIN}}*/}
-                                    <span className={styles['string-content']}>
+                                    <span className={cx('string-content')}>
                                         {value}
                                     </span>
-                                    {/*<br/>*/}
                                 </>
                             </Tag>
                     else
                         return <span key={name}>
-                                <span
-                                    // style={{marginLeft: (level+1)*MARGIN}}
-                                        className={styles['string-content']}>
-                                        {value}
+                                <span className={cx('string-content')}>
+                                    {value}
                                 </span>
-                                {/*<br/>*/}
                             </span>
                 return <Tag
                             key={'tag' + name}
@@ -96,6 +101,7 @@ import Alert from "@mui/material/Alert";
                             level={level}>
                     <BuildObject nodes={Object.entries(value).filter(en => en[0] !== ATTRIBUTE_SIGN)}
                                  level={level + 1}
+                                 xpaths={xpaths}
                                  parent={[...parent, name]}/>
                 </Tag>
             }
@@ -132,11 +138,11 @@ import Alert from "@mui/material/Alert";
     }
 
 
-
-
 const File = ({xmlContent, xPaths}) => {
     const [xmlNodes, setXmlNodes] = useState(false)
     const [error, setError] = useState(false)
+    const [load, setLoad] = useState(false)
+    const [evv, setEvv] = useState([])
 
     useEffect(() => {
         setError(false)
@@ -146,62 +152,22 @@ const File = ({xmlContent, xPaths}) => {
                     console.error('Error parsing XML:', err);
                     setError(err)
                 } else {
-                    console.log(result)
                     setXmlNodes(result)
-                    // const builder = new Builder()
-                    // setXmlContent(builder.buildObject(result))
                 }
             });
         if(xmlContent) {
+            setLoad(true)
             const parser = new DOMParser();
             const doc = parser.parseFromString(xmlContent, "application/xml");
-            console.log(doc)
-
-            // const expresion = '/*/cac:ProcurementProjectLot/[cbc:ID/@schemeName=\'Lot\']/cac:TenderingTerms/cac:PostAwardProcess'
-
-            const expresion = '/*/ext:UBLExtensions'
-
             const namespaces = extractNamespaces(doc);
 
             const nsResolver = (prefix) => {
               return namespaces[prefix] || null;
             }
-            console.log('namespaces',namespaces)
-            // const rss = SaxonJS.XPath.evaluate('/*/cac:ProcurementProjectLot[cbc:ID/@schemeName=\'Lot\']/cac:TenderingTerms/cac:PostAwardProcess',doc)
-            // console.log(res)
-            // SaxonJS.getResource({
-            //     file:xmlContent,
-            //     type:'xml'
-            // })
-            //     .then(doc => console.log(SaxonJS.XPath.evaluate('/*/cac:ProcurementProjectLot[cbc:ID/@schemeName=\'Lot\']/cac:TenderingTerms/cac:PostAwardProcess',doc)))
-            //
-            // const res = doc.evaluate('/*/cac:AdditionalDocumentReference',doc,null,XPathResult.ANY_TYPE,null)
-            // console.log(res)
-
-            // const evaluator = new XPathEvaluator()
-            // const expresion = evaluator.createExpression('/*/cac:AdditionalDocumentReference')
-            // const res = expresion.evaluate(doc,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
-            // document.querySelector("output").textContent = res.snapshotLength;
-            // const ex = doc.evaluate("/cbc\\:WebsiteURI", doc, null, XPathResult.ANY_TYPE)
-            // console.log(ex)
-            //
-            // const namespaces = {
-            //     'cbc':"urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2",
-            //     'cac':"urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2",
-            // };
-
-
-            // const nsResolver = (prefix) => {
-            //   return namespaces[prefix] || null;
-            // }
-
-            // const result = doc.evaluate(expresion, doc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-            // console.log(result)
 
             let evaluatedNamespaces = []
 
             xPaths.forEach(xPath => {
-                console.log(xPath.xpath)
                 try {
                     const evaluated = doc.evaluate(
                         xPath.xpath,
@@ -215,21 +181,19 @@ const File = ({xmlContent, xPaths}) => {
                       for (let i = 0; i < evaluated.snapshotLength; i++) {
                         const node = evaluated.snapshotItem(i);
                         const absoluteXPath = getAbsoluteXPath(node);
-                        // console.log(`Node found: ${node.textContent}`);
-                        // console.log(`Absolute XPath: ${absoluteXPath}`);
                          evaluatedNamespaces.push(absoluteXPath)
                       }
                     } else {
-                      console.log('No nodes found.');
+                      // console.log('No nodes found.');
                     }
 
                 } catch (err) {
                     console.log(err)
                 }
-                    console.log(evaluatedNamespaces)
+                setLoad(false)
+                setEvv(evaluatedNamespaces)
 
             })
-            // console.log(evaluatedNamespaces)
         }
     }, [xmlContent])
 
@@ -237,9 +201,11 @@ const File = ({xmlContent, xPaths}) => {
 
     return (
         <>
+            {load && <CircularProgress/>}
             {error && <Alert severity="error">{error.message}</Alert>}
             {!!xmlNodes && !error && <BuildObject nodes={Object.entries(xmlNodes)}
                                         level={0}
+                                                  xpaths={evv}
                                         parent={[]}/>}
         </>
     )
