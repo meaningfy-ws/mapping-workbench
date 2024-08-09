233deb9c7ac079eda2af03f34089e88c616f92b1
Kolea PLESCO <kaleanych@UNIVERSE.local>
2024-08-10 00:07:57 +0300

    Updated https redirect behaviour for traefik

--- mapping_workbench/backend/core/entrypoints/api/main.py
+++ mapping_workbench/backend/core/entrypoints/api/main.py
@@ -46,7 +46,7 @@ app = FastAPI(
     swagger_ui_oauth2_redirect_url=f"{ROOT_API_PATH}/docs/oauth2-redirect"
 )
 
-origins = [f"{settings.HOST}:{settings.PORT}", f"*.mw.{settings.SUBDOMAIN}{settings.DOMAIN}", "*"]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -92,7 +92,7 @@ secured_routers: list = [
     config_routes.router,
     ontology_routes.router,
     task_manager_routes.router,
-    tasks_routes.router, # Depricated
+    tasks_routes.router,  # Deprecated
     fields_registry.router,
     xsd_schema_router.router
 ]
@@ -115,6 +115,8 @@ if not settings.is_env_production():
                                  stack_trace=exception_traceback)
         return JSONResponse(
             status_code=500,
-            content={"detail": [{"msg": f"Exception name: {exception.__class__.__name__} Error: {exception_traceback}"}]},
+            content={
+                "detail": [{"msg": f"Exception name: {exception.__class__.__name__} Error: {exception_traceback}"}]
+            },
             headers={"Access-Control-Allow-Origin": "*"}  # Allow all is a temporary solution
         )
