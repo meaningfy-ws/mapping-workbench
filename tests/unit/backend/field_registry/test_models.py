ec2fad2b3093e0e5585e44df0a39b067e5bd28a3
Dumitru <d.prijilevschi@gmail.com>
2024-09-06 12:05:26 +0300

    make parrent node optional, also relative path

--- tests/unit/backend/field_registry/test_models.py
+++ tests/unit/backend/field_registry/test_models.py
@@ -1,4 +1,6 @@
+import pytest
 from beanie import PydanticObjectId
+from pydantic import ValidationError
 
 from mapping_workbench.backend.fields_registry.models.field_registry import StructuralElement, StructuralElementIn
 
@@ -17,3 +19,14 @@ def test_structural_element_general_in(dummy_project_link):
 
     # In model should be convertable into base model
     StructuralElement.model_validate(test_structural_element)
+
+
+def test_structural_element_general_in_mandatory_fields(dummy_project_link):
+    with pytest.raises(ValidationError):
+        StructuralElementIn()
+
+    StructuralElementIn(
+        id=str(PydanticObjectId()),
+        label="Dummy ST In",
+        absolute_xpath="/*/dummy/absolute/xpath",
+    )
