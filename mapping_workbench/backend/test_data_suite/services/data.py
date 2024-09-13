bbbee1b0c30808fae1ff61461431bd252352282c
Kolea PLESCO <kaleanych@UNIVERSE.local>
2024-09-13 20:07:36 +0300

    Updated test data transform within the Triple Map form

--- mapping_workbench/backend/test_data_suite/services/data.py
+++ mapping_workbench/backend/test_data_suite/services/data.py
@@ -32,3 +32,14 @@ async def get_test_data_file_resources_for_package(package_id: PydanticObjectId)
 
 def is_valid_test_data_format(test_format: str):
     return test_format in [e.value for e in TestDataFileResourceFormat]
+
+
+async def get_mapping_package_id_for_test_data_file_resource(
+        test_data_file_resource: TestDataFileResource
+) -> PydanticObjectId:
+    package_id = None
+    if test_data_file_resource.test_data_suite:
+        test_data_suite: TestDataSuite = await test_data_file_resource.test_data_suite.fetch()
+        if isinstance(test_data_suite, TestDataSuite) and test_data_suite.mapping_package_id:
+            package_id = test_data_suite.mapping_package_id
+    return package_id
