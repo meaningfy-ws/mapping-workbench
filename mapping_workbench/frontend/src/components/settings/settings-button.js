46248183bc9650e9b88c98171701c92891f70b7d
Victor Frunze <silverberserk@gmail.com>
2024-09-16 22:02:45 +0300

    MWB-789: move icon right

--- mapping_workbench/frontend/src/components/settings/settings-button.js
+++ mapping_workbench/frontend/src/components/settings/settings-button.js
@@ -4,42 +4,55 @@ import Box from '@mui/material/Box';
 import ButtonBase from '@mui/material/ButtonBase';
 import SvgIcon from '@mui/material/SvgIcon';
 import Tooltip from '@mui/material/Tooltip';
+import {useTheme} from "@mui/material/styles";
+import {useEffect} from "react";
 
-export const SettingsButton = (props) => (
-  <Tooltip title="Settings">
-    <Box
-      sx={{
-        backgroundColor: 'background.paper',
-        borderRadius: '50%',
-        bottom: 56,
-        boxShadow: 16,
-        margin: (theme) => theme.spacing(4),
-        position: 'fixed',
-        right: 0,
-        opacity: 0.4,
-        transition: 'linear 0.3s',
-        zIndex: (theme) => theme.zIndex.speedDial,
-          ':hover': {
-            opacity: 1
-          }
-      }}
-      {...props}>
-      <ButtonBase
-        sx={{
-          backgroundColor: 'primary.main',
-          borderRadius: '50%',
-          color: 'primary.contrastText',
-          p: '10px'
-        }}
-      >
-        <SvgIcon>
-          <Settings03Icon />
-        </SvgIcon>
-      </ButtonBase>
-    </Box>
-  </Tooltip>
-);
+export const SettingsButton = (props) => {
+    const theme = useTheme()
+    useEffect(() => {
+        console.log(theme)
+        const widget = document.querySelector('#jsd-widget')
+        widget.contentDocument.body.style = `background-color:transparent;`
+        widget.style = 'left: 32px; bottom:65px; position:fixed; border:none; z-index:1210; background-color:transparent'
+
+    }, [theme]);
+
+    return (
+        <Tooltip title="Settings">
+            <Box
+                sx={{
+                    backgroundColor: 'background.paper',
+                    borderRadius: '50%',
+                    bottom: 56,
+                    boxShadow: 16,
+                    margin: (theme) => theme.spacing(4),
+                    position: 'fixed',
+                    right: 0,
+                    opacity: 0.4,
+                    transition: 'linear 0.3s',
+                    zIndex: (theme) => theme.zIndex.speedDial,
+                    ':hover': {
+                        opacity: 1
+                    }
+                }}
+                {...props}>
+                <ButtonBase
+                    sx={{
+                        backgroundColor: 'primary.main',
+                        borderRadius: '50%',
+                        color: 'primary.contrastText',
+                        p: '10px'
+                    }}
+                >
+                    <SvgIcon>
+                        <Settings03Icon/>
+                    </SvgIcon>
+                </ButtonBase>
+            </Box>
+        </Tooltip>
+    )
+};
 
 SettingsButton.propTypes = {
-  onClick: PropTypes.func
+    onClick: PropTypes.func
 };
