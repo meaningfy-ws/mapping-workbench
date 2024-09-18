af4cf82ccd96ade65b09a5b62d3124bc9c885830
Victor Frunze <silverberserk@gmail.com>
2024-09-18 17:53:50 +0300

    MWB-796: update mapping package

--- mapping_workbench/frontend/src/pages/app/mapping-packages/[id]/view.js
+++ mapping_workbench/frontend/src/pages/app/mapping-packages/[id]/view.js
@@ -1,35 +1,34 @@
 import {useEffect, useState} from 'react';
+import dynamic from "next/dynamic";
+
 import ArrowLeftIcon from '@untitled-ui/icons-react/build/esm/ArrowLeft';
-import Chip from '@mui/material/Chip';
-import Divider from '@mui/material/Divider';
-import Grid from '@mui/material/Unstable_Grid2';
-import Link from '@mui/material/Link';
-import Stack from '@mui/material/Stack';
-import SvgIcon from '@mui/material/SvgIcon';
 import Tab from '@mui/material/Tab';
+import Chip from '@mui/material/Chip';
+import Link from '@mui/material/Link';
 import Tabs from '@mui/material/Tabs';
+import Stack from '@mui/material/Stack';
+import Divider from '@mui/material/Divider';
+import SvgIcon from '@mui/material/SvgIcon';
 import Typography from '@mui/material/Typography';
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
 
 import {paths} from 'src/paths';
 import {Seo} from 'src/components/seo';
-import {Layout as AppLayout} from 'src/layouts/app';
-import {mappingPackagesApi as sectionApi} from 'src/api/mapping-packages';
-import {RouterLink} from 'src/components/router-link';
 import {useRouter} from "src/hooks/use-router";
-import {FileResourceCollectionsCard} from 'src/sections/app/file-manager/file-resource-collections-card'
+import {Layout as AppLayout} from 'src/layouts/app';
+import {RouterLink} from 'src/components/router-link';
+import {mappingPackagesApi as sectionApi} from 'src/api/mapping-packages';
+import MappingPackageDetails from "src/sections/app/mapping-package/details";
 
-import {PropertyList} from "src/components/property-list";
-import {PropertyListItem} from "src/components/property-list-item";
-import {shaclTestSuitesApi} from "src/api/shacl-test-suites";
+const Resources =
+    dynamic(() => import("src/sections/app/mapping-package/resources"));
+const MappingPackageRulesView =
+    dynamic(() => import("src/sections/app/mapping-package/mapping-package-rules-view"));
+const TripleMapping =
+    dynamic(() => import("src/sections/app/mapping-package/triple-mapping"));
+const StatesView =
+    dynamic(() => import("src/sections/app/mapping-package/state/states_view"));
 
-import StatesView from "src/sections/app/mapping-package/state/states_view";
-import MappingPackageRulesView from "src/sections/app/mapping-package/mapping-package-rules-view";
-import TripleMapping from "src/sections/app/mapping-package/triple-mapping";
-import {sparqlTestSuitesApi} from "../../../../api/sparql-test-suites";
-import {resourceCollectionsApi} from "../../../../api/resource-collections";
-import {testDataSuitesApi} from "../../../../api/test-data-suites";
+
 
 const tabs = [
     {label: 'Details', value: 'details'},
@@ -51,11 +50,9 @@ const Page = () => {
             .then(res => setItem(res))
     }, [id]);
 
-    const handleTabsChange = (event, value) => {
-        setCurrentTab(value);
-    }
+    const handleTabsChange = (event, value) => setCurrentTab(value);
 
-    if(!item)
+    if (!item)
         return
 
     return (
@@ -133,124 +130,10 @@ const Page = () => {
                     <Divider/>
                 </Stack>
                 {currentTab === 'details' && (
-                    <Grid container
-                          spacing={3}>
-                        <Grid md={12}
-                              xs={12}>
-                            <Card>
-                                <CardContent>
-                                    <Grid
-                                        item={item}
-                                        md={12}
-                                        xs={12}
-                                    >
-                                        <PropertyList>
-                                            <PropertyListItem
-                                                label="Description"
-                                                value={item.description}
-                                                sx={{
-                                                    whiteSpace: "pre-wrap",
-                                                    px: 3,
-                                                    py: 1.5
-                                                }}
-                                            />
-                                            <Divider/>
-                                            <Grid container
-                                                  spacing={3}>
-                                                <Grid md={6}
-                                                      xs={12}>
-                                                    <PropertyListItem
-                                                        label="Identifier"
-                                                        value={item.identifier}
-                                                    />
-                                                </Grid>
-                                                <Grid md={12}
-                                                      xs={12}>
-                                                    <PropertyListItem
-                                                        label="Mapping Version"
-                                                        value={item.mapping_version}
-                                                    />
-                                                </Grid>
-                                                <Grid md={12}
-                                                      xs={12}>
-                                                    <PropertyListItem
-                                                        label="EPO Version"
-                                                        value={item.epo_version}
-                                                    />
-                                                </Grid>
-                                                <Grid md={12}
-                                                      xs={12}>
-                                                    <PropertyListItem
-                                                        label="eForms Subtype"
-                                                        value={item.eform_subtypes.join(', ')}
-                                                    />
-                                                </Grid>
-                                                <Grid md={12}
-                                                      xs={12}>
-                                                    <PropertyListItem
-                                                        label="Start Date"
-                                                        value={item.start_date}
-                                                    />
-                                                </Grid>
-                                                <Grid md={12}
-                                                      xs={12}>
-                                                    <PropertyListItem
-                                                        label="End Date"
-                                                        value={item.end_date}
-                                                    />
-                                                </Grid>
-                                                <Grid md={12}
-                                                      xs={12}>
-                                                    <PropertyListItem
-                                                        label="eForms XSD version"
-                                                        value={item.eforms_sdk_versions.join(', ')}
-                                                    />
-                                                </Grid>
-                                            </Grid>
-                                        </PropertyList>
-                                    </Grid>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    </Grid>
+                    <MappingPackageDetails item={item}/>
                 )}
                 {currentTab === 'resources' && (
-                    <Grid container
-                          spacing={3}>
-                        <Grid md={12}
-                              xs={12}>
-                            <FileResourceCollectionsCard
-                                collectionApi={testDataSuitesApi}
-                                filters={{
-                                    mapping_package_id: item._id
-                                }}
-                            />
-                            <Divider sx={{m: 3}}/>
-                            <FileResourceCollectionsCard
-                                collectionApi={shaclTestSuitesApi}
-                                filters={{
-                                    ids: ((item.shacl_test_suites || []).length > 0
-                                        && item.shacl_test_suites.map(x => x.id)) || ''
-                                }}
-                            />
-                            <Divider sx={{m: 3}}/>
-                            <FileResourceCollectionsCard
-                                collectionApi={sparqlTestSuitesApi}
-                                filters={{
-                                    ids: ((item.sparql_test_suites || []).length > 0
-                                        && item.sparql_test_suites.map(x => x.id)) || ''
-                                }}
-                            />
-                            <Divider sx={{m: 3}}/>
-                            <FileResourceCollectionsCard
-                                collectionApi={resourceCollectionsApi}
-                                filters={{
-                                    ids: ((item.resource_collections || []).length > 0
-                                        && item.resource_collections.map(x => x.id)) || ''
-                                }}
-                            />
-                        </Grid>
-                    </Grid>
+                    <Resources item={item}/>
                 )}
                 {currentTab === "mappingRules" && (
                     <MappingPackageRulesView id={id}/>
---
+++ mapping_workbench/frontend/src/sections/app/mapping-package/details.js
@@ -0,0 +1,94 @@
+import Card from "@mui/material/Card";
+import Divider from "@mui/material/Divider";
+import CardContent from "@mui/material/CardContent";
+import Grid from "@mui/material/Unstable_Grid2";
+
+import {PropertyList} from "../../../components/property-list";
+import {PropertyListItem} from "../../../components/property-list-item";
+
+const MappingPackageDetails = ({item}) => {
+    return (
+        <Grid container
+              spacing={3}>
+            <Grid md={12}
+                  xs={12}>
+                <Card>
+                    <CardContent>
+                        <Grid
+                            item={item}
+                            md={12}
+                            xs={12}
+                        >
+                            <PropertyList>
+                                <PropertyListItem
+                                    label="Description"
+                                    value={item.description}
+                                    sx={{
+                                        whiteSpace: "pre-wrap",
+                                        px: 3,
+                                        py: 1.5
+                                    }}
+                                />
+                                <Divider/>
+                                <Grid container
+                                      spacing={3}>
+                                    <Grid md={6}
+                                          xs={12}>
+                                        <PropertyListItem
+                                            label="Identifier"
+                                            value={item.identifier}
+                                        />
+                                    </Grid>
+                                    <Grid md={12}
+                                          xs={12}>
+                                        <PropertyListItem
+                                            label="Mapping Version"
+                                            value={item.mapping_version}
+                                        />
+                                    </Grid>
+                                    <Grid md={12}
+                                          xs={12}>
+                                        <PropertyListItem
+                                            label="EPO Version"
+                                            value={item.epo_version}
+                                        />
+                                    </Grid>
+                                    <Grid md={12}
+                                          xs={12}>
+                                        <PropertyListItem
+                                            label="eForms Subtype"
+                                            value={item.eform_subtypes.join(', ')}
+                                        />
+                                    </Grid>
+                                    <Grid md={12}
+                                          xs={12}>
+                                        <PropertyListItem
+                                            label="Start Date"
+                                            value={item.start_date}
+                                        />
+                                    </Grid>
+                                    <Grid md={12}
+                                          xs={12}>
+                                        <PropertyListItem
+                                            label="End Date"
+                                            value={item.end_date}
+                                        />
+                                    </Grid>
+                                    <Grid md={12}
+                                          xs={12}>
+                                        <PropertyListItem
+                                            label="eForms XSD version"
+                                            value={item.eforms_sdk_versions.join(', ')}
+                                        />
+                                    </Grid>
+                                </Grid>
+                            </PropertyList>
+                        </Grid>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default MappingPackageDetails
