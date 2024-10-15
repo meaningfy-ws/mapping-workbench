eea535e0b8a5acd8da4c6d46d165fd5932cb681d
Victor Frunze <silverberserk@gmail.com>
2024-10-15 09:32:45 +0300

    MWB-837: add version label

--- mapping_workbench/frontend/src/contexts/globalState/globalState-provider.js
+++ mapping_workbench/frontend/src/contexts/globalState/globalState-provider.js
@@ -1,10 +1,18 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import {initialState, GlobalStateContext} from './globalState-context';
+import {versionApi as sectionApi} from "../../api/version";
+import {useAuth} from "../../hooks/use-auth";
 
 export const GlobalStateProvider = ({children}) => {
     const [state, setState] = useState(initialState);
+    const {isAuthenticated} = useAuth();
 
+    useEffect(() => {
+        isAuthenticated && sectionApi.getItems()
+            .then(res => handleGlobalStateUpdate('version',res.version))
+            .catch(err => console.error(err))
+    },[isAuthenticated])
 
     const handleGlobalStateUpdate = (item, value) => {
         setState(prevState => ({...prevState, [item]: value}))
