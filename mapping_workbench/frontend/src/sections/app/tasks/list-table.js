4f8b048fa7dbf22157973351eaf31b768658c2d5
Victor Frunze <silverberserk@gmail.com>
2024-10-21 09:54:13 +0300

    MWB-839: update styles

--- mapping_workbench/frontend/src/sections/app/tasks/list-table.js
+++ mapping_workbench/frontend/src/sections/app/tasks/list-table.js
@@ -1,38 +1,38 @@
 import {Fragment, useState} from 'react';
 import PropTypes from 'prop-types';
+import moment from "moment";
 
+import InfoIcon from '@mui/icons-material/Info';
+import CancelIcon from '@mui/icons-material/Cancel';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
-import CancelIcon from '@mui/icons-material/Cancel';
-import InfoIcon from '@mui/icons-material/Info';
-import ChevronDownIcon from '@untitled-ui/icons-react/build/esm/ChevronDown';
-import ChevronRightIcon from '@untitled-ui/icons-react/build/esm/ChevronRight';
-import CardContent from '@mui/material/CardContent';
-import Divider from '@mui/material/Divider';
-import Grid from '@mui/material/Grid';
-import IconButton from '@mui/material/IconButton';
-import SvgIcon from '@mui/material/SvgIcon';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Typography from '@mui/material/Typography';
-import Button from "@mui/material/Button";
 
+import List from '@mui/material/List';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Table from '@mui/material/Table';
+import Button from '@mui/material/Button';
+import SvgIcon from '@mui/material/SvgIcon';
+import Popover from '@mui/material/Popover';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import TableRow from '@mui/material/TableRow';
+import TableHead from '@mui/material/TableHead';
+import TableCell from '@mui/material/TableCell';
+import TableBody from '@mui/material/TableBody';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import CardContent from '@mui/material/CardContent';
+
+import nl2br from "src/utils/nl2br";
 import {Scrollbar} from 'src/components/scrollbar';
-import {SeverityPill} from "../../../components/severity-pill";
-import TablePagination from "../../components/table-pagination";
-import timeTransformer from "../../../utils/time-transformer";
-import {useGlobalState} from "../../../hooks/use-global-state";
-import TableSorterHeader from "../../components/table-sorter-header";
-import moment from "moment";
-import nl2br from "../../../utils/nl2br";
-import ListItem from "@mui/material/ListItem";
-import List from "@mui/material/List";
-import Popover from "@mui/material/Popover";
-import {Box} from "@mui/system";
-import Stack from "@mui/material/Stack";
+import timeTransformer from "src/utils/time-transformer";
+import {useGlobalState} from "src/hooks/use-global-state";
+import {SeverityPill} from "src/components/severity-pill";
+import TablePagination from "src/sections/components/table-pagination";
+import TableSorterHeader from "src/sections/components/table-sorter-header";
+
 
 export const taskStatuses = {
     QUEUED: "QUEUED",
@@ -169,12 +169,10 @@ export const ListTable = (props) => {
                         {items.map((item) => {
                             const item_id = item.task_id;
                             const isCurrent = item_id === currentItem;
-
                             return (
                                 <Fragment key={item_id}>
-                                    <TableRow
-                                        hover
-                                        key={item_id}
+                                    <TableRow hover
+                                              key={item_id}
                                     >
                                         <TableCell
                                             padding="checkbox"
@@ -195,8 +193,11 @@ export const ListTable = (props) => {
                                             width="25%"
                                         >
                                             <IconButton onClick={() => handleItemToggle(item_id)}>
-                                                <SvgIcon>
-                                                    {isCurrent ? <ChevronDownIcon/> : <ChevronRightIcon/>}
+                                                <SvgIcon sx={{
+                                                    transform: isCurrent && 'rotate(90deg)',
+                                                    transition: '0.2s linear'
+                                                }}>
+                                                    <ChevronRightIcon/>
                                                 </SvgIcon>
                                             </IconButton>
                                         </TableCell>
@@ -217,13 +218,7 @@ export const ListTable = (props) => {
                                         <TableCell>
                                             {timeTransformer(item.finished_at, timeSetting)}
                                         </TableCell>
-                                        {/*<TableCell>*/}
-                                        {/*    {item.finished_at ? moment.utc(moment(item.finished_at).diff(moment(item.started_at))).format("HH:mm:ss") : '-'}*/}
-                                        {/*</TableCell>*/}
                                         <TableCell align="left">
-                                            {/*<SeverityPill color={mapStatusColor(item.task_status)}>*/}
-                                            {/*    {item.task_status}*/}
-                                            {/*</SeverityPill>*/}
                                             <Stack onMouseEnter={(event) => handlePopoverEnter(event, item)}
                                                    onMouseLeave={handlePopoverLeave}>
                                                 <SvgIcon>
@@ -254,76 +249,73 @@ export const ListTable = (props) => {
                                             </Button>
                                         </TableCell>
                                     </TableRow>
-                                    {
-                                        isCurrent && (
-                                            <TableRow>
-                                                <TableCell
-                                                    colSpan={9}
-                                                    sx={{
-                                                        p: 0,
-                                                        position: 'relative',
-                                                        '&:after': {
-                                                            position: 'absolute',
-                                                            content: '" "',
-                                                            top: 0,
-                                                            left: 0,
-                                                            backgroundColor: 'primary.main',
-                                                            width: 3,
-                                                            height: 'calc(100% + 1px)'
-                                                        }
-                                                    }}
-                                                >
-                                                    <CardContent>
-                                                        <Grid
-                                                            container
-                                                            spacing={3}
-                                                            direction="column"
-                                                            gap={3}
-                                                        >
-                                                            {item.exception_message && <>
+                                    {isCurrent && (
+                                        <TableRow>
+                                            <TableCell
+                                                colSpan={9}
+                                                sx={{
+                                                    p: 0,
+                                                    position: 'relative',
+                                                    '&:after': {
+                                                        position: 'absolute',
+                                                        content: '" "',
+                                                        top: 0,
+                                                        left: 0,
+                                                        backgroundColor: 'primary.main',
+                                                        width: 3,
+                                                        height: 'calc(100% + 1px)'
+                                                    }
+                                                }}
+                                            >
+                                                <CardContent>
+                                                    <Grid
+                                                        container
+                                                        spacing={3}
+                                                        direction="column"
+                                                        gap={3}
+                                                    >
+                                                        {item.exception_message && <>
+                                                            <Typography sx={{pl: 3, pt: 1}}
+                                                                        variant="h6"
+                                                                        color="error">
+                                                                Message
+                                                            </Typography>
+                                                            <Divider/>
+                                                            <Typography sx={{pl: 3}}>
+                                                                {nl2br(item.exception_message)}
+                                                            </Typography>
+                                                        </>}
+                                                        {item.warnings && !!item.warnings.length &&
+                                                            <>
                                                                 <Typography sx={{pl: 3, pt: 1}}
                                                                             variant="h6"
-                                                                            color="error">
-                                                                    Message
+                                                                            color="orange">
+                                                                    Warning
                                                                 </Typography>
                                                                 <Divider/>
-                                                                <Typography sx={{pl: 3}}>
-                                                                    {nl2br(item.exception_message)}
-                                                                </Typography>
+                                                                <List sx={{pl: 3}}>
+                                                                    {item.warnings.map((warning, key) =>
+                                                                        <ListItem
+                                                                            key={'warning' + key}>
+                                                                            {warning}
+                                                                        </ListItem>
+                                                                    )}
+                                                                </List>
                                                             </>}
-                                                            {item.warnings && !!item.warnings.length &&
-                                                                <>
-                                                                    <Typography sx={{pl: 3, pt: 1}}
-                                                                                variant="h6"
-                                                                                color="orange">
-                                                                        Warning
-                                                                    </Typography>
-                                                                    <Divider/>
-                                                                    <List sx={{pl: 3}}>
-                                                                        {item.warnings.map((warning, key) =>
-                                                                            <ListItem
-                                                                                key={'warning' + key}>
-                                                                                {warning}
-                                                                            </ListItem>
-                                                                        )}
-                                                                    </List>
-                                                                </>}
-                                                        </Grid>
-                                                    </CardContent>
-                                                    <Divider/>
-                                                </TableCell>
-                                            </TableRow>
-                                        )
-                                    }
+                                                    </Grid>
+                                                </CardContent>
+                                                <Divider/>
+                                            </TableCell>
+                                        </TableRow>
+                                    )}
                                 </Fragment>
-                            )
-                                ;
+                            );
                         })}
                     </TableBody>
                 </Table>
                 <Popover
                     id="mouse-over-popover"
-                    sx={{pointerEvents: 'none', cursor: 'pointer'}}
+                    sx={{pointerEvents: 'none'}}
                     open={!!popoverShow?.anchor}
                     anchorEl={popoverShow?.anchor}
                     anchorOrigin={{
@@ -332,7 +324,7 @@ export const ListTable = (props) => {
                     }}
                     transformOrigin={{
                         vertical: 'top',
-                        horizontal: 'left',
+                        horizontal: 'right',
                     }}
                     onClose={handlePopoverLeave}
                     disableRestoreFocus
