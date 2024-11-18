c2ff4c57107ba395de562656002c11eda3667e31
Dumitru <d.prijilevschi@gmail.com>
2024-11-18 16:52:00 +0200

    encrease task timeout to 2 hours

--- mapping_workbench/backend/config/__init__.py
+++ mapping_workbench/backend/config/__init__.py
@@ -89,7 +89,7 @@ class TaskManagerSettings(BaseSettings):
     def TASK_MANAGER_MAX_WORKERS(self, config_value: str) -> int:
         return int(config_value)
 
-    @env_property(config_key='MW_TASK_TIMEOUT', default_value="2000")
+    @env_property(config_key='MW_TASK_TIMEOUT', default_value="7200")
     def TASK_TIMEOUT(self, config_value: str) -> int:
         return int(config_value)
 
