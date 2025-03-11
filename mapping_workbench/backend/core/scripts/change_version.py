241153f8f889ad76e3002c6232168a00165321dc
Kolea PLESCO <kaleanych@UNIVERSE.local>
2025-03-11 19:29:47 +0200

    feat: added tracking for users and some activities

--- mapping_workbench/backend/core/scripts/change_version.py
+++ mapping_workbench/backend/core/scripts/change_version.py
@@ -136,7 +136,7 @@ def finish_gitflow_release():
     version = get_current_release_version()
 
     print(f"Commiting the changes 'release/{version}'...")
-    run_command(["git", "commit", "-m", f"release/{version}"])
+    run_command(["git", "commit", "-a", "-m", f"release/{version}"])
 
     # Push the release branch to the remote
     print(f"Publishing the release branch 'release/{version}'...")
