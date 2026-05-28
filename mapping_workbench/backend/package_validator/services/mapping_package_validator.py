48767d6d64fd5a73f4dfd3aa1cf0c07b020f8e5b
Kolea PLESCO <kaleanych@UNIVERSE.local>
2026-05-28 12:52:46 +0300

    updated gridfs file downloader to stream data

--- mapping_workbench/backend/package_validator/services/mapping_package_validator.py
+++ mapping_workbench/backend/package_validator/services/mapping_package_validator.py
@@ -80,7 +80,7 @@ async def get_state_test_data_suite_validatiton(mapping_package_state,
     state: MappingPackageState = await get_specific_mapping_package_state(mapping_package_state.id)
     test_data_suite = next((
         test_data_suite for test_data_suite in state.test_data_suites
-        if test_data_suite.oid == test_data_suite_id), False
+        if test_data_suite.oid == str(test_data_suite_id)), False
     )
     if not test_data_suite:
         raise ResourceNotFoundException(404)
@@ -93,14 +93,14 @@ async def get_state_test_data_validatiton(mapping_package_state, test_data_suite
     state: MappingPackageState = await get_specific_mapping_package_state(mapping_package_state.id)
     test_data_suite = next((
         test_data_suite for test_data_suite in state.test_data_suites
-        if test_data_suite.oid == test_data_suite_id), False
+        if test_data_suite.oid == str(test_data_suite_id)), False
     )
     if not test_data_suite:
         raise ResourceNotFoundException(404)
 
     test_data = next((
         test_data for test_data in test_data_suite.test_data_states
-        if test_data.oid == test_data_id), False
+        if test_data.oid == str(test_data_id)), False
     )
     if not test_data:
         raise ResourceNotFoundException(404)
